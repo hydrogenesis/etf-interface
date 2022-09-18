101a154630be00ddda6593b8a4c68b0272cbf4f1
mickeydota <mickeydota@gmail.com>
2022-09-18 10:03:02 +0800

    explorer

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -349,10 +349,10 @@ export const WRAPPED_NATIVE_CURRENCY: { [chainId: number]: Token | undefined } =
   [SupportedChainId.MAINNET]: new Token(
     SupportedChainId.MAINNET,
     '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-    18, 
+    18,
     'WETH',
     'Wrapped Ether'
-  ),  
+  ),
   [SupportedChainId.OPTIMISM]: new Token(
     SupportedChainId.OPTIMISM,
     '0x4200000000000000000000000000000000000006',
